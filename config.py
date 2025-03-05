18f01dcbe0ef9116e5bda21035acbe4638dd7bc3
Madrine07 <denladunamis@gmail.com>
2025-03-05 10:36:33 -0800

    simple changes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/flask_authors_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/authors_db'
     JWT_SECRET_KEY = "author"
     
     #this is the connection string file.
